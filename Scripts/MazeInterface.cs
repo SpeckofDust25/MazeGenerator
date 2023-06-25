f5075836ccd7cd77e9078aaed5dcd928e4a2c929
SpeckofDust25 <120860234+SpeckofDust25@users.noreply.github.com>
2023-06-25 05:28:13 -0500

    Fixed External Editor Issue

--- Scripts/MazeInterface.cs
+++ Scripts/MazeInterface.cs
@@ -3,125 +3,124 @@ using System;
 
 public partial class MazeInterface : Panel
 {
-    //Nodes
-    private TextureRect n_maze_image;
-    private Button n_maximize;
-    private Button n_minimize;
-    private Button n_center;
-    private HSlider n_magnify_slider;
-    private Label n_magnify_label;
+	//Nodes
+	private TextureRect n_maze_image;
+	private Button n_maximize;
+	private Button n_minimize;
+	private Button n_center;
+	private HSlider n_magnify_slider;
+	private Label n_magnify_label;
 
-    //Variables
-    private bool panning = false;
-    private Vector2 panning_vec;
-    private bool is_focused;
-    private float magnify_increment = 0.10f;
+	//Variables
+	private bool panning = false;
+	private Vector2 panning_vec;
+	private bool is_focused;
+	private float magnify_increment = 0.10f;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        SetupNodes();
-        SetupConnections();
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		SetupNodes();
+		SetupConnections();
 
+		panning_vec = new Vector2();
+		FocusMode = FocusModeEnum.All;
+	}
 
-        panning_vec = new Vector2();
-        FocusMode = FocusModeEnum.All;
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		HandleInput();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        HandleInput();
-    }
+	public override void _GuiInput(InputEvent inputEvent)
+	{
+		if (inputEvent is InputEventMouseMotion newEvent) //Mouse Movement
+		{
+			panning_vec.X = newEvent.Relative.X;
+			panning_vec.Y = newEvent.Relative.Y;
+		}
+	}
 
-    public override void _GuiInput(InputEvent inputEvent)
-    {
-        if (inputEvent is InputEventMouseMotion newEvent) //Mouse Movement
-        {
-            panning_vec.X = newEvent.Relative.X;
-            panning_vec.Y = newEvent.Relative.Y;
-        }
-    }
+	public void HandleInput()
+	{
+		//Panning Image
+		if (Input.IsActionPressed("panning")) {
+			if (is_focused) { //Stay Focused Outside Panel Range
+				n_maze_image.Position += panning_vec;
+			} else if (GetGlobalRect().HasPoint(GetGlobalMousePosition())) {
+				is_focused = true;
+				n_maze_image.Position += panning_vec;
+			} 
+		} else {
+			is_focused = false;
+		}
 
-    public void HandleInput()
-    {
-        //Panning Image
-        if (Input.IsActionPressed("panning")) {
-            if (is_focused) { //Stay Focused Outside Panel Range
-                n_maze_image.Position += panning_vec;
-            } else if (GetGlobalRect().HasPoint(GetGlobalMousePosition())) {
-                is_focused = true;
-                n_maze_image.Position += panning_vec;
-            } 
-        } else {
-            is_focused = false;
-        }
+		panning_vec = Vector2.Zero;
+	}
 
-        panning_vec = Vector2.Zero;
-    }
+	private void SetupNodes()
+	{
+		n_maze_image = GetNode<TextureRect>("MazeImage");
+		n_maximize = GetNode<Button>("MazeImageInterface/MaximizeButton");
+		n_minimize = GetNode<Button>("MazeImageInterface/MinimizeButton");
+		n_center = GetNode<Button>("MazeImageInterface/CenterButton");
+		n_magnify_slider = GetNode<HSlider>("MazeImageInterface/MagnifySlider");
+		n_magnify_label = GetNode<Label>("MazeImageInterface/MagnifyLabel");
+	}
 
-    private void SetupNodes()
-    {
-        n_maze_image = GetNode<TextureRect>("MazeImage");
-        n_maximize = GetNode<Button>("MazeImageInterface/MaximizeButton");
-        n_minimize = GetNode<Button>("MazeImageInterface/MinimizeButton");
-        n_center = GetNode<Button>("MazeImageInterface/CenterButton");
-        n_magnify_slider = GetNode<HSlider>("MazeImageInterface/MagnifySlider");
-        n_magnify_label = GetNode<Label>("MazeImageInterface/MagnifyLabel");
-    }
+	private void SetupConnections()
+	{
+		Callable c_maximize = new Callable(this, "_maximize_pressed");
+		n_maximize.Connect("pressed", c_maximize);
 
-    private void SetupConnections()
-    {
-        Callable c_maximize = new Callable(this, "_maximize_pressed");
-        n_maximize.Connect("pressed", c_maximize);
+		Callable c_minimize = new Callable(this, "_minimize_pressed");
+		n_minimize.Connect("pressed", c_minimize);
 
-        Callable c_minimize = new Callable(this, "_minimize_pressed");
-        n_minimize.Connect("pressed", c_minimize);
+		Callable c_center = new Callable(this, "_center_pressed");
+		n_center.Connect("pressed", c_center);
 
-        Callable c_center = new Callable(this, "_center_pressed");
-        n_center.Connect("pressed", c_center);
+		Callable c_magnify_slider = new Callable(this, "_magnify_slider_changed");
+		n_magnify_slider.Connect("value_changed", c_magnify_slider);
+	}
 
-        Callable c_magnify_slider = new Callable(this, "_magnify_slider_changed");
-        n_magnify_slider.Connect("value_changed", c_magnify_slider);
-    }
+	public void _maximize_pressed()
+	{
+		if (n_maze_image.Scale.X < 4 - magnify_increment) {
+			n_maze_image.Scale += new Vector2(magnify_increment, magnify_increment);
+		}
 
-    public void _maximize_pressed()
-    {
-        if (n_maze_image.Scale.X < 4 - magnify_increment) {
-            n_maze_image.Scale += new Vector2(magnify_increment, magnify_increment);
-        }
+		UpdateMagnifyIndicators();
+	}
 
-        UpdateMagnifyIndicators();
-    }
+	public void _minimize_pressed()
+	{
+		if (n_maze_image.Scale.X > 0.00) {
+			n_maze_image.Scale -= new Vector2(magnify_increment, magnify_increment);
+		}
 
-    public void _minimize_pressed()
-    {
-        if (n_maze_image.Scale.X > 0.00) {
-            n_maze_image.Scale -= new Vector2(magnify_increment, magnify_increment);
-        }
+		UpdateMagnifyIndicators();
+	}
 
-        UpdateMagnifyIndicators();
-    }
+	public void UpdateMagnifyIndicators(bool update_slider = true)
+	{
+		n_magnify_label.Text = string.Format("{0:N0}", n_maze_image.Scale.X * 100) + "%";
+		
+		if (update_slider) {
+			n_magnify_slider.Value = n_maze_image.Scale.X * 100;
+		}
+	}
 
-    public void UpdateMagnifyIndicators(bool update_slider = true)
-    {
-        n_magnify_label.Text = string.Format("{0:N0}", n_maze_image.Scale.X * 100) + "%";
-        
-        if (update_slider) {
-            n_magnify_slider.Value = n_maze_image.Scale.X * 100;
-        }
-    }
+	public void _center_pressed()
+	{
+		Vector2 center = GetGlobalRect().GetCenter() - n_maze_image.GetRect().Size / 2;
+		n_maze_image.GlobalPosition = center;
+	}
 
-    public void _center_pressed()
-    {
-        Vector2 center = GetGlobalRect().GetCenter() - n_maze_image.GetRect().Size / 2;
-        n_maze_image.GlobalPosition = center;
-    }
-
-    public void _magnify_slider_changed(float value)
-    {
-        n_maze_image.Scale = new Vector2(value, value) * 0.01f;
-        UpdateMagnifyIndicators(false);
-    }
+	public void _magnify_slider_changed(float value)
+	{
+		n_maze_image.Scale = new Vector2(value, value) * 0.01f;
+		UpdateMagnifyIndicators(false);
+	}
 
 }
